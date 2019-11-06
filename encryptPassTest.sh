54b3c4e9618c9aa7d2925dfc9b3c2ca3473de916
Robin Greig <robin.greig@calalta.com>
2019-11-06 04:39:35 -0700

    updated sandbox

--- encryptPassTest.sh
+++ encryptPassTest.sh
@@ -4,8 +4,14 @@
 #read -p 'SAIT Username: ' uservar
 #bash -c "echo 'identity=\"$uservar\"'"
 read -p 'SAIT Password: ' passvar
-bash -c "echo 'password=\"$passvar\"'"
-read -ra newpass <<< "echo -n $passvar | iconv -t utf16le | openssl md4"
+#bash -c "echo 'password= $passvar'"
+encryptpass=`echo -n $passvar | iconv -t utf16le | openssl md4`
+#read -ra newpass <<< "echo -n $passvar | iconv -t utf16le | openssl md4"
 #bash -c "echo -n $passvar | iconv -t utf16le | openssl md4"
-bash -c "echo 'newpass=\"$newpass\"'"
-
+#bash -c "echo 'encryptpass=\"$encryptpass\"'"
+#bash -c "echo 'encryptpass= $encryptpass'"
+IFS=' '
+read -ra hash <<< "$encryptpass"
+#bash -c "echo 'stripped hash= ${hash[1]}'"
+hashonly=${hash[1]}
+bash -c "echo 'hash variable= $hashonly'"
